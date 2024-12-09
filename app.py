6fed6bd69007f27c3f8e52d4d49c61bfcaae49dc
rizkaastengels <rizkyadityabgr@gmail.com>
2024-12-09 10:05:12 +0700

    Update aplikasi kalkulator.py

--- app.py
+++ app.py
@@ -1,14 +1,3 @@
-import streamlit as st
-
-st.title('Welcome to Streamlit Development Class!')
-st.write('We will learning how to create Web Apps using Python programming with Streamlit Library')
-st.warning('Stay Tune!')
-st.success('Almost there!')
-st.header('A...')
-st.subheader('B...')
-st.balloons()
-st.success('Done!')
-st.write("Hello, *World!* :sunglasses:")
 
 import streamlit as st
 
